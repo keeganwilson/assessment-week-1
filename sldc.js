32cc4f879cc2a345e3863412e0e77869568c43a6
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-25 10:10:45 -0600

    completed sdlc portion

--- sldc.js
+++ sldc.js
@@ -5,17 +5,21 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
-
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is a step that is worked on by a number of departments within the company to determine how to proceed and whether it is feasible and possible in the allotted budget." 
+const design = "This step develops and portrays a blueprint of the product, which helps to specify the hardware and requirements of your system as well as assist in crafting a significant architecture of your system."
+const implementation = "This is the step where developers start writing code with the end goal of creating a prototype using specific tools and techniques."
+const testingAndIntegration = "Once the prototype has been developed it needs to be tested before deployment. This step is test engineers check for bugs and run-time errors. They also check to make sure the product is working as per the requirements. Once the test engineers are satisfied the product can be delpoyed or implemented."
+const maintain = "Test engineers can do the best they can but inevitably some issues pop up after implemention. Developers are then charged with fixing the issues and testing the results."
